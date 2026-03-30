d0ec424348f850a26230fa159d537f111f631dd0
User <user@PC-PF5R059V.bbrouter>
2026-03-30 11:35:29 +0200

    add .env config, rework init script

--- clearing_solana/scripts/init.ts
+++ clearing_solana/scripts/init.ts
@@ -1,8 +1,11 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { ClearingSolana } from "../target/types/clearing_solana";
+import { Keypair } from "@solana/web3.js";
+import "dotenv/config"
 
 import idl from "../target/idl/clearing_solana.json";
+import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
 
 const provider = anchor.AnchorProvider.env();
 anchor.setProvider(provider);
@@ -12,16 +15,32 @@ const program = new Program(
     provider
 );
 
+const privateKey = process.env.ADMIN_PRIVATE_KEY;
+
+const adminKeypair = Keypair.fromSecretKey(bs58.decode(privateKey));
+
 async function main() {
-    const authority = provider.wallet.publicKey;
+    const authority = adminKeypair.publicKey;
 
     console.log("Authority:", authority.toBase58());
+
+    const connection = provider.connection;
+
+    const signature = await connection.requestAirdrop(
+        authority,
+        100 * anchor.web3.LAMPORTS_PER_SOL
+    );
+
+    await connection.confirmTransaction(signature);
+
+    console.log("Airdropped 100 SOL");
 
     await program.methods
         .initClearingState()
         .accounts({
             authority,
         })
+        .signers([adminKeypair])
         .rpc();
 
     console.log("State initialized");
@@ -29,6 +48,7 @@ async function main() {
     await program.methods
         .initAdmin()
         .accounts({ authority })
+        .signers([adminKeypair])
         .rpc();
 
     console.log(`Admin created: ${authority}`);
@@ -38,6 +58,7 @@ async function main() {
         .accounts({
             authority,
         })
+        .signers([adminKeypair])
         .rpc();
 
     console.log("Escrow initialized");
@@ -47,6 +68,7 @@ async function main() {
         .accounts({
             authority,
         })
+        .signers([adminKeypair])
         .rpc();
 
     console.log("Pool manager initialized");
